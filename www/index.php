693638e937a8f478db523861258cb861bcec8a4c
KaySar12 <shintheman12@gmail.com>
2024-10-11 09:06:08 +0700

    update: add source of fossbilling

--- www/index.php
+++ www/index.php
@@ -1,75 +1,95 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
-        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
-        <link rel="stylesheet" href="/assets/css/bulma.min.css">
-    </head>
-    <body>
-        <section class="hero is-medium is-info is-bold">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h1 class="title">
-                        LAMP STACK
-                    </h1>
-                    <h2 class="subtitle">
-                        Your local development environment
-                    </h2>
-                </div>
-            </div>
-        </section>
-        <section class="section">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Environment</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><?= apache_get_version(); ?></li>
-                                <li>PHP <?= phpversion(); ?></li>
-                                <li>
-                                    <?php
-                                    $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+<?php
 
-/* check connection */
-                                    if (mysqli_connect_errno()) {
-                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
-                                    } else {
-                                        /* print server version */
-                                        printf("MySQL Server %s", mysqli_get_server_info($link));
-                                    }
-                                    /* close connection */
-                                    mysqli_close($link);
-                                    ?>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><a href="/phpinfo.php">phpinfo()</a></li>
-                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
-                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
-                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <footer class="footer">
-            <div class="content has-text-centered">
-                <p>
-                    <strong><a href="https://www.sprintcube.com" target="_blank">SprintCube</a></strong><br>
-                    The source code is released under the <a href="https://github.com/sprintcube/docker-compose-lamp/blob/master/LICENSE" target="_blank">MIT license</a>.
-                </p>
-            </div>
-        </footer>
-    </body>
-</html>
+/**
+ * Copyright 2022-2024 FOSSBilling
+ * Copyright 2011-2021 BoxBilling, Inc.
+ * SPDX-License-Identifier: Apache-2.0.
+ *
+ * @copyright FOSSBilling (https://www.fossbilling.org)
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache-2.0
+ */
+require_once __DIR__ . '/load.php';
+$di = include __DIR__ . '/di.php';
+
+// Setting up the debug bar
+$debugBar = new DebugBar\StandardDebugBar();
+$debugBar['request']->useHtmlVarDumper();
+$debugBar['messages']->useHtmlVarDumper();
+
+$config = FOSSBilling\Config::getConfig();
+$config['info']['salt'] = '********';
+$config['db'] = array_fill_keys(array_keys($config['db']), '********');
+
+$configCollector = new DebugBar\DataCollector\ConfigCollector($config);
+$configCollector->useHtmlVarDumper();
+
+$debugBar->addCollector($configCollector);
+
+// Now move onto the actual process of setting up the app & routing
+$url = $_GET['_url'] ?? $_SERVER['PATH_INFO'] ?? '';
+$http_err_code = $_GET['_errcode'] ?? null;
+
+if ($url === '/run-patcher') {
+    $patcher = new FOSSBilling\UpdatePatcher();
+    $patcher->setDi($di);
+
+    try {
+        $patcher->applyConfigPatches();
+        $patcher->applyCorePatches();
+        $di['tools']->emptyFolder(PATH_CACHE);
+
+        exit('Any missing config migrations or database patches have been applied and the cache has been cleared');
+    } catch (Exception $e) {
+        exit('An error occurred while attempting to apply patches: <br>' . $e->getMessage());
+    }
+}
+
+$debugBar['time']->startMeasure('session_start', 'Starting / restoring the session');
+/*
+ * Workaround: Session IDs get reset when using PGs like PayPal because of the `samesite=strict` cookie attribute, resulting in the client getting logged out.
+ * Internally the return and cancel URLs get a restore_session GET parameter attached to them with the proper session ID to restore, so we do so here.
+ */
+if (!empty($_GET['restore_session'])) {
+    session_id($_GET['restore_session']);
+}
+
+$di['session'];
+$debugBar['time']->stopMeasure('session_start');
+
+if (strncasecmp($url, ADMIN_PREFIX, strlen(ADMIN_PREFIX)) === 0) {
+    define('ADMIN_AREA', true);
+    $appUrl = str_replace(ADMIN_PREFIX, '', preg_replace('/\?.+/', '', $url));
+    $app = new Box_AppAdmin([], $debugBar);
+} else {
+    define('ADMIN_AREA', false);
+    $appUrl = $url;
+    $app = new Box_AppClient([], $debugBar);
+}
+
+$app->setUrl($appUrl);
+$app->setDi($di);
+
+$debugBar['time']->startMeasure('translate', 'Setting up translations');
+$di['translate']();
+$debugBar['time']->stopMeasure('translate');
+
+// If HTTP error code has been passed, handle it.
+if (!is_null($http_err_code)) {
+    switch ($http_err_code) {
+        case '404':
+            $e = new FOSSBilling\Exception('Page :url not found', [':url' => $url], 404);
+            echo $app->show404($e);
+
+            break;
+        default:
+            $http_err_code = intval($http_err_code);
+            http_response_code($http_err_code);
+            $e = new FOSSBilling\Exception('HTTP Error :err_code occurred while attempting to load :url', [':err_code' => $http_err_code, ':url' => $url], $http_err_code);
+            echo $app->render('error', ['exception' => $e]);
+    }
+    exit;
+}
+
+// If no HTTP error passed, run the app.
+echo $app->run();
+exit;
